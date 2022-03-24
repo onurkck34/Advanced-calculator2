2f4f040431b692e698bb01bca57616836395a7a5
Onur Koçak <98240992+onurkck34@users.noreply.github.com>
2022-03-24 23:13:10 +0300

    Add files via upload

---
+++ advancedcalculator2.py
@@ -0,0 +1,38 @@
+def topla(sayi1,sayi2):
+    return sayi1 + sayi2
+def cikar(sayi1,sayi2):
+    return sayi1-sayi2
+def carp(sayi1,sayi2):
+    return sayi1*sayi2
+def bol(sayi1,sayi2):
+    return sayi1/sayi2
+print("Operasyon?")
+print("1 : Topla")
+print("2 : Cikar")
+print("3 : Carp")
+print("4 : Bol")
+
+secenek = input("Operasyon Seciminiz : ")
+
+if secenek > '4':
+    print("Islem Gecersizdir")
+if secenek < '1':
+    print("Islem Gecersizdir")
+
+sayi1 = int(input("1.Sayiyi giriniz :"))
+sayi2 = int(input("2.Sayiyi giriniz:"))
+
+if secenek == '1':
+    print("Toplam : " + str(topla(sayi1,sayi2)))
+
+elif secenek == '2':
+    print("Cikarma : " + str(cikar(sayi1,sayi2)))
+
+elif secenek == '3':
+    print("Carpma : " + str(carp(sayi1,sayi2)))
+
+elif secenek == '4':
+    print("Bolme : " + str(bol(sayi1,sayi2)))
+
+else:
+    print("Gecersiz Secenek")
